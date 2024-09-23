ec5c831a250ace4958677079c44149aa0920c9df
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-09-23 14:23:35 -0400

    request work

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // server.mjs
 import { createServer } from 'node:http';
 import { SerialPort, ReadlineParser } from 'serialport';
+import { readFile } from 'node:fs/promises';
 
-const server = createServer((req, res) => {
+
+const server = createServer(async (req, res) => {
 
     console.log(`Received ${req.method} request for: ${req.url}`);
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
+    handleRequest(req, res);
+
+    const htmlContent = await readFile('index.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlContent);
 });
+
+const handleRequest = (req, res) => {
+    
+    if(req.url === '/START?'){
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Game Started!\n');
+    }
+}
 
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
@@ -34,3 +47,4 @@ port.write('Hello Arduino\n', (err) => {
 port.on('error', (err) => {
     console.log('Serial port error: ', err.message);
 })
+
--- utils/gptFunctions.js
+++ utils/gptFunctions.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 const openai = new OpenAI();
 
-const completion = await openai.chat.completions.create({
+export const completion = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
         { role: "system", content: "You are a helpful assistant." },
